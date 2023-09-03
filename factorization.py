caba92ccb19f3bba54b25f4e9819e4950c8c9bc1
diixo <anatolii.podoba@gmail.com>
2023-09-03 07:51:12 +0300

    factorization.py initial commit.

---
+++ factorization.py
@@ -0,0 +1,11 @@
+
+import sys
+
+def factorize(*number):
+   # YOUR CODE HERE
+   raise NotImplementedError()
+
+if __name__ == "__main__":
+   argList = sys.argv[1:]
+   if len(argList) > 0:
+      factorize()
