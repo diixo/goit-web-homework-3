e1b5284bb110e167354947c252580fd7d01e85fb
diixo <anatolii.podoba@gmail.com>
2023-09-03 17:35:16 +0300

    update

--- factorization.py
+++ factorization.py
@@ -3,9 +3,14 @@ import sys
 
 def factorize(*number):
    # YOUR CODE HERE
+   #return tuple([i for i in number])
    raise NotImplementedError()
 
 if __name__ == "__main__":
-   argList = sys.argv[1:]
-   if len(argList) > 0:
-      factorize()
+   a, b, c, d  = factorize(128, 255, 99999, 10651060)
+
+   assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+   assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+   assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+   assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 
+      304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
