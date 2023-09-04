08b3b3ec653fc30d63caa1a00b3ea70b37dc205b
diixo <anatolii.podoba@gmail.com>
2023-09-04 10:44:43 +0300

    update factorization-queue

--- factorization-queue.py
+++ factorization-queue.py
@@ -1,47 +1,49 @@
 
-from multiprocessing import Process, current_process, RLock, Value, Array
+from multiprocessing import Process, Queue, current_process, cpu_count, RLock, Value, Array
 from ctypes import c_int
 import logging
 from array import array
+import time
 
 logger = logging.getLogger()
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
-calc_array = []
 
-def calculate(num: int, lock: RLock, lst: list):
-   global calc_array
+
+def calculate(num: int):
    result = []
 
    for i in range(1, num + 1):
       if num % i == 0:
          result.append(i)
-   
-   with lock:
-      lst.append(result)
-   print(f"pid={current_process().pid}, {num:} {result}")
+   return result
+
 
 def factorize(*numbers):
-   global calc_array
    sz = len(numbers)
-
-   lock = RLock()
 
    processes = []
    for i in range(sz):
-      pr = Process(target=calculate, args=(numbers[i], lock))
+      pr = Process(target=calculate, args=(numbers[i],))
       pr.start()
       processes.append(pr)
 
    [pr.join() for pr in processes]
 
-   return tuple(arr for arr in calc_array)
+   return tuple()
+
 
 if __name__ == "__main__":
+   print(f"Count CPU: {cpu_count()}")
+   start = time.perf_counter()
 
    a, b, c, d = factorize(128, 255, 99999, 10651060)
 
    exit(0)
+
+   finish = time.perf_counter()
+
+   print(a, b, c, d)
 
    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
@@ -49,4 +51,4 @@ if __name__ == "__main__":
    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 
       304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 
-   print("OK")
+   print(f"OK: duration={round(finish-start, 3)}")
